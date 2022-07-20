60a9690c0c5a752aad3aba145a52b3bfa0cbecbc
Gabriel Maguna <gabimaguna@gmail.com>
2022-07-20 09:24:27 -0300

    intro JS

---
+++ intro.js
@@ -0,0 +1,18 @@
+const name = "Gabriel";
+const age = 22;
+const partner = false;
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const skillers = ["Diego", "Celeste", "Ian", "Gonzalo", "Melina"];
+const InfoSkillsFac = {
+  name: "Gabriel",
+  age: 22,
+  partner: false,
+  numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  skillers: ["Diego", "Celeste", "Ian", "Gonzalo", "Melina"],
+};
+console.log("Mi nombre: ",name)
+console.log("Mi edad: ", age)
+console.log("Estoy en pareja? :", partner)
+console.log("Numeros :", numbers)
+console.log("Skillers :", skillers)
+console.log("Objeto :", InfoSkillsFac)
